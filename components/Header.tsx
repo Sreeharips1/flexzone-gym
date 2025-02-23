0f08b80ea8198574aee8e4e9331cda66b22e3a42
sreeharipshaiju <sreehariwsree@gmail.com>
2025-02-24 01:09:20 +0530

    linkupdated

--- components/Header.tsx
+++ components/Header.tsx
@@ -47,7 +47,7 @@ const Header = () => {
         {/* Right Section: Login Button + Mobile Menu Button */}
         <div className="flex items-center gap-4">
           {/* Login Button */}
-          <Link href="http://localhost:3001/"
+          <Link href="http://localhost:3000/"
             className="bg-red-600 text-white px-4 py-2 rounded-full font-medium transition duration-300 hover:bg-red-700 ">
             Login
           </Link>
